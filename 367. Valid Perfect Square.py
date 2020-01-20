10bc0fc5bb8105ca2ba99564651aadbabb8ca74b
Vats_vk <vibhasvats.india@gmail.com>
2020-01-19 20:16:09 -0500

    Add files via upload

---
+++ 367. Valid Perfect Square.py
@@ -0,0 +1,23 @@
+def perfectSquare( num):
+    if num == 1:
+        return True
+    if num < 1:
+        return False
+    if num % 10 not in (1, 4, 5, 6, 9, 0):
+        return False
+    # now will do binary search.
+    start = 1
+    end = num
+    while start <= end:
+        mid_num = start + (end - start)//2
+        if mid_num * mid_num == num:
+            return True
+        elif mid_num * mid_num > num:
+            end = mid_num - 1
+        elif mid_num * mid_num < num:
+            start = mid_num + 1
+    return False
+
+
+
+print(perfectSquare(104976))
