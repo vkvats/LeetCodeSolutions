b7dc986a1e0c3cf9e658d4f86a36934d0c98da02
Vats_vk <vibhasvats.india@gmail.com>
2020-01-19 23:52:35 -0500

    Update 69. Sqrt(x).py
    there is this other method names Newton method, which gives very fast solutions.

--- 69. Sqrt(x).py
+++ 69. Sqrt(x).py
@@ -13,3 +13,14 @@ def mysqrt( x):
         elif mid_num * mid_num < x:
             start = mid_num + 1
     return start - 1
+
+
+## Newton method to find square ( in decimals)  root of an integer
+
+def Newton_sqrt(x):
+    if x < 2:
+        return x
+    guess = x/2
+    while abs(x - guess * guess) > 0.01:
+        guess = 0.5*(guess + x/guess)
+    return guess
