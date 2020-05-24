4d2f770e3af82d0fdb5c12f5afd0d704b2c6729c
Vibhas <vkvats@iu.edu>
2020-05-24 11:58:28 -0400

    solution of 1207

--- inProgress/1207_uniqueNumberOfOccurances.py
+++ inProgress/1207_uniqueNumberOfOccurances.py
@@ -1,20 +1,17 @@
 
-def uniqueOccurrences(arr: List[int]) -> bool:
-    pass
+def uniqueOccurrences(arr) -> bool:
+    flag = False
+    frequency_dict = {}
+    for key in arr:
+        frequency_dict[key] = frequency_dict.get(key, 0) + 1
+    if len(frequency_dict.values()) == len(set(frequency_dict.values())):
+        flag = True
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return flag
 
 if __name__ == '__main__':
-    arr = [1, 2, 2, 1, 1, 3]
+    # arr = [1, 2, 2, 1, 1, 3]
+    # arr = [1, 2]
+    # arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]
+    arr = []
     print(uniqueOccurrences(arr))
