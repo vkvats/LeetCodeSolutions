0d743d9946812ab6dcede8f5269ffe6b5efb7e46
Vibhas <vkvats@iu.edu>
2020-05-30 16:54:34 -0400

    1189 solution

---
+++ inProgress/1189. Maximum Number of Balloons.py
@@ -0,0 +1,31 @@
+def maxNumberOfBalloons(text):
+    count = {}
+    num_of_balloon =0
+    for alpha in text:
+        count[alpha] = count.get(alpha,0) +1
+    while True:
+        for alpha2 in "balloon":
+            if alpha2 in count:
+                count[alpha2] -=1
+                if count[alpha2] ==-1:
+                    return num_of_balloon
+            else:
+                return num_of_balloon
+        num_of_balloon +=1
+
+# best solution on leetcode
+from collections import Counter
+def maxNumberOfBalloonsBest(text):
+    counter = Counter(text)
+    b = counter['b']
+    a = counter['a']
+    l = counter['l'] // 2
+    o = counter['o'] // 2
+    n = counter['n']
+    return min(b, a, l, o, n)
+
+if __name__ == '__main__':
+    text = "nlaebolko"
+    text = "loonbalxballpoon"
+    text = "lloo"
+    print(maxNumberOfBalloons(text))
