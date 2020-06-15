a7d57c473d704f1d81f6195a4503cf3fc3accdde
Vibhas <vkvats@iu.edu>
2020-06-15 12:56:55 -0400

    added best solution

--- inProgress/1480. Running Sum of 1d Array.py
+++ inProgress/1480. Running Sum of 1d Array.py
@@ -6,6 +6,12 @@ def runningsum(nums):
         sum += num
         output.append(sum)
     return output
+## best solution from leetcode
+def runningSumBest(nums):
+    for i in range(1, len(nums)):
+        nums[i] += nums[i - 1]
+    return nums
+
 
 if __name__ == '__main__':
     # nums= [1,2,3,4]
